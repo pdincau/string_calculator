4b39e5aec94208a1170d76c199302179efa07f37
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-07-29 13:58:12 +0200

    Refactoring code

--- lib/string_calculator.ex
+++ lib/string_calculator.ex
@@ -1,10 +1,10 @@
 defmodule StringCalculator do
 
-  def add(""), do: 0
-
   def add(string) do
     string |> String.split(["\n", ","]) |> sum
   end
+
+  defp sum([""]), do: 0
 
   defp sum(list), do: sum(list, 0)
 
